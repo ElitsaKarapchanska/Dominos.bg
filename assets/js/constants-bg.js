6f508c313a4b2a8dd42868ea45ce60026c3b1008
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-02-11 14:32:24 +0200

    Started work on constants file

---
+++ assets/js/constants-bg.js
@@ -0,0 +1,97 @@
+// menu
+const DEALS
+const PIZZAS
+const STARTERS
+const CHICKEN
+const PASTA
+const SALADS
+const SANDWICH
+const DIPS
+const DESSERTS
+const DRINKS
+
+// tags
+const VEGETARIAN = 'ЗА ВЕГЕТАРИАНЦИ';
+const SPICY = 'ПИКАНТНО';
+const FASTING = 'ПОСТНИ';
+const NEW = 'НОВО';
+const DESSERTS = 'ДЕСЕРТИ';
+const ICE_CREAMS = 'СЛАДОЛЕДИ';
+
+// pizza types
+const MARGHERITA
+const RIVERONI
+const BEAST_V2
+const CHICKENITA
+const DOMINOS_SPECIAL
+const CHICK_CHI_RICK
+const CARBONARA
+const AMERICAN_HOT
+const GARDEN_CLASSIC
+const PEPPERONI_CLASSIC
+const BARBECUE_CHICKEN
+const BARBECUE_CLASSIC
+const NEW_YORK
+const HAM_CLASSIC
+const ITALIAN_CLASSIC
+const HAWAII
+const FOUR_CHEESES
+const TUNA_PIZZA
+const MEAT_MANIA
+const EXTRAVAGANZA
+const BURGER_PIZZA
+const MASTER_BURGER_PIZZA
+const FASTING_V2
+const RANCHERA
+const PIZZA_OSLO_TOMATO
+const PIZZA_OSLO_CREAM
+const PIZZA_GAUCHO
+
+// ingredients
+const SAUCES = 'Сосове';
+const TOMATO_SAUCE = 'Доматен сос';
+const CREAM = 'Сметана';
+const BBQ = 'Барбекю сос';
+
+const HERBS = 'Подправки';
+const PARMESAN_SPRINKLES = 'Пармезан снежинки';
+const BASIL = 'Босилек';
+const OREGANO = 'Риган';
+
+const CHEESES = 'Сирена';
+const EMMENTAL = 'Ементал';
+const FETA = 'Краве сирене';
+const MOZZARELLA = 'Моцарела';
+const SMOKED_CHEESE = 'Топено сирене';
+const PARMESAN = 'Пармезан';
+const VEGAN_MOZZARELLA = 'Веган Моцарела';
+const CHEDDAR = 'Чедар сирене';
+
+const MEATS = 'Меса';
+const PULLED_BEEF = 'Дърпано телешко';
+const SPICY_BEEF = 'Пикантно телешко';
+const SMOCKED_HAM = 'Пушена шунка';
+const SMOCKED_BACON = 'Пушен бекон';
+const CHORISO = 'Чоризо';
+const VENTRICHINA = 'Вентричина';
+const CHICKEN = 'Пиле';
+const PEPPERONI = 'Пеперони';
+const TUNA = 'Риба тон';
+
+const VEGETABLES = 'Зеленчуци';
+const ARUGULA = 'Рукола';
+const PICKLES = 'Кисeли краставички';
+const CORN = 'Царевица';
+const ONION = 'Лук';
+const BLACK_OLIVES = 'Черни маслини';
+const PINAPPLE = 'Ананас';
+const FRESH_TOMATOES = 'Пресни домати';
+const FRESH_MUSHROOMS = 'Пресни гъби';
+const FRESH_GREEN_PEPPERS = 'Пресни зелени чушки';
+const JALAPENO_PEPPER = 'Халапеньо-люти чушки';
+
+const MISC = 'Смесен';
+const PESTO = 'Песто сос';
+const CARAMELIZED_ONIONS = 'Карамелизиран лук';
+
+const EXTRA = 'Допълнително';
