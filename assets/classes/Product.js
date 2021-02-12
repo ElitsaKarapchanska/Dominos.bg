719f9fb1b60cb3e7c0c06857d4a9b851721c7e7b
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-02-12 20:13:19 +0200

    creating class Product

---
+++ assets/classes/Product.js
@@ -0,0 +1,7 @@
+// super class for all products(pizzas,chicken,desserts,drinks,pasta,salads,sandwiches,sauces,starters);
+class Product{
+    constructor(title,image){
+        this.title=title;
+        this.image=image;
+    }
+}
