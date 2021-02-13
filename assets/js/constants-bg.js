6552405b66fd957d0d8deff991ec024f646a1930
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-02-13 12:02:30 +0200

    Refactored the constants into objects

--- assets/js/constants-bg.js
+++ assets/js/constants-bg.js
@@ -1,158 +1,190 @@
-// header
-const TEL = "070012525";
-const PIZZA_TRACKER = "PIZZA TRACKER";
-const EMPTY_CART = "Вашата кошница е празна";
+const HEADER_CONSTANTS = {
+  TEL: "070012525",
+  PIZZA_TRACKER: "PIZZA TRACKER",
+  EMPTY_CART: "Вашата кошница е празна",
+};
 
-// footer
-const MENU = "МЕНЮ";
-const RESTAURANTS = "РЕСТОРАНТИ";
-const CONTACT_US = "СВЪРЖЕТЕ СЕ С НАС";
-const TERMS_AND_CONDITIONS = "УСЛОВИЯ ЗА ПОЛЗВАНЕ";
-const ALLERGENS = "АЛЕРГЕНИ";
-const JOB_APPLICATIONS = "КАНДИДАТСТВАНЕ ЗА РАБОТА";
-const DATA_PROTECTION_POLICY = "ПОЛИТИКА ЗА ЗАЩИТА НА ЛИЧНИТЕ ДАННИ";
+const FOOTER_CONSTANTS = {
+  MENU: "МЕНЮ",
+  RESTAURANTS: "РЕСТОРАНТИ",
+  CONTACT_US: "СВЪРЖЕТЕ СЕ С НАС",
+  TERMS_AND_CONDITIONS: "УСЛОВИЯ ЗА ПОЛЗВАНЕ",
+  ALLERGENS: "АЛЕРГЕНИ",
+  JOB_APPLICATIONS: "КАНДИДАТСТВАНЕ ЗА РАБОТА",
+  DATA_PROTECTION_POLICY: "ПОЛИТИКА ЗА ЗАЩИТА НА ЛИЧНИТЕ ДАННИ",
+};
 
-// menu
-const DEALS = "DEALS";
-const PIZZAS = "ПИЦА";
-const STARTERS = "ПРЕДЯСТИЯ";
-const CHICKEN = "ПИЛЕ";
-const PASTA = "ПАСТА";
-const SALADS = "САЛАТИ";
-const SANDWICH = "САНДВИЧИ";
-const DIPS = "СОС";
-const DESSERTS = "ДЕСЕРТИ";
-const DRINKS = "НАПИТКИ";
+const MENU_CONSTANTS = {
+  DEALS: "DEALS",
+  PIZZAS: "ПИЦА",
+  STARTERS: "ПРЕДЯСТИЯ",
+  CHICKEN: "ПИЛЕ",
+  PASTA: "ПАСТА",
+  SALADS: "САЛАТИ",
+  SANDWICH: "САНДВИЧИ",
+  DIPS: "СОС",
+  DESSERTS: "ДЕСЕРТИ",
+  DRINKS: "НАПИТКИ",
+};
 
-// tags for sorting
-const VEGETARIAN = "ЗА ВЕГЕТАРИАНЦИ";
-const SPICY = "ПИКАНТНО";
-const FASTING = "ПОСТНИ";
-const NEW = "НОВО";
-const DESSERTS = "ДЕСЕРТИ";
-const ICE_CREAMS = "СЛАДОЛЕДИ";
+const SORTING_TAGS_CONSTANTS = {
+  VEGETARIAN: "ЗА ВЕГЕТАРИАНЦИ",
+  SPICY: "ПИКАНТНО",
+  FASTING: "ПОСТНИ",
+  NEW: "НОВО",
+  DESSERTS: "ДЕСЕРТИ",
+  ICE_CREAMS: "СЛАДОЛЕДИ",
+};
 
-// buttons
-const CHOOSE = "ИЗБЕРЕТЕ";
-const ORDER_NOW = "ПОРЪЧАЙ СЕГА";
-const SOLD_OUT = "ИЗЧЕРПАНО";
-const ADD = "ДОБАВИ";
+const BUTTONS_CONSTANTS = {
+  CHOOSE: "ИЗБЕРЕТЕ",
+  ORDER_NOW: "ПОРЪЧАЙ СЕГА",
+  SOLD_OUT: "ИЗЧЕРПАНО",
+  ADD: "ДОБАВИ",
+};
 
-// pizzas
-const CUSTOM_PIZZA = "НАПРАВИ ПИЦА ПО ТВОЙ ВКУС";
-const HALF_AND_HALF = "ПОЛОВИН–ПОЛОВИН";
-const MARGHERITA = "МАРГАРИТА";
-const RIVERONI = "РИВЕРОНИ";
-const BEAST_V2 = "ЗВЕРСКА V2";
-const CHICKENITA = "ЧИКЕНИТА";
-const DOMINOS_SPECIAL = "ДОМИНОС СПЕЦИАЛНА";
-const CHICK_CHI_RICK = "ЧИК –ЧИ –РИК";
-const CARBONARA = "КАРБОНАРА";
-const AMERICAN_HOT = "АМЕРИКАН ХОТ";
-const GARDEN_CLASSIC = "ГАРДЪН КЛАСИК";
-const PEPPERONI_CLASSIC = "ПЕПЕРОНИ КЛАСИК";
-const BARBECUE_CHICKEN = "БАРБЕКЮ ПИЛЕ";
-const BARBECUE_CLASSIC = "БАРБЕКЮ КЛАСИК";
-const NEW_YORK = "НЮ ЙОРК";
-const HAM_CLASSIC = "ШУНКА КЛАСИК";
-const ITALIAN_CLASSIC = "ИТАЛИАНСКА";
-const HAWAII = "ХАВАЙ";
-const FOUR_CHEESES = "ФОРМАДЖИ";
-const TUNA_PIZZA = "ПИЦА ТОН";
-const MEAT_MANIA = "MEAT MANIA";
-const EXTRAVAGANZA = "ЕКСТРАВАГАНЗА";
-const BURGER_PIZZA = "БУРГЕР ПИЦА";
-const MASTER_BURGER_PIZZA = "МАСТЪР БУРГЕР ПИЦА";
-const FASTING_V2 = "ПОСТНА V.2";
-const RANCHERA = "РАНЧЕРА";
-const PIZZA_OSLO_TOMATO = "";
-const PIZZA_OSLO_CREAM = "ПИЦА ОСЛО СЪС СМЕТАНА";
-const PIZZA_GAUCHO = "ПИЦА ГАУЧО";
+const PIZZA_NAMES_CONSTANTS = {
+  CUSTOM_PIZZA: "НАПРАВИ ПИЦА ПО ТВОЙ ВКУС",
+  HALF_AND_HALF: "ПОЛОВИН–ПОЛОВИН",
+  MARGHERITA: "МАРГАРИТА",
+  RIVERONI: "РИВЕРОНИ",
+  BEAST_V2: "ЗВЕРСКА V2",
+  CHICKENITA: "ЧИКЕНИТА",
+  DOMINOS_SPECIAL: "ДОМИНОС СПЕЦИАЛНА",
+  CHICK_CHI_RICK: "ЧИК –ЧИ –РИК",
+  CARBONARA: "КАРБОНАРА",
+  AMERICAN_HOT: "АМЕРИКАН ХОТ",
+  GARDEN_CLASSIC: "ГАРДЪН КЛАСИК",
+  PEPPERONI_CLASSIC: "ПЕПЕРОНИ КЛАСИК",
+  BARBECUE_CHICKEN: "БАРБЕКЮ ПИЛЕ",
+  BARBECUE_CLASSIC: "БАРБЕКЮ КЛАСИК",
+  NEW_YORK: "НЮ ЙОРК",
+  HAM_CLASSIC: "ШУНКА КЛАСИК",
+  ITALIAN_CLASSIC: "ИТАЛИАНСКА",
+  HAWAII: "ХАВАЙ",
+  FOUR_CHEESES: "ФОРМАДЖИ",
+  TUNA_PIZZA: "ПИЦА ТОН",
+  MEAT_MANIA: "MEAT MANIA",
+  EXTRAVAGANZA: "ЕКСТРАВАГАНЗА",
+  BURGER_PIZZA: "БУРГЕР ПИЦА",
+  MASTER_BURGER_PIZZA: "МАСТЪР БУРГЕР ПИЦА",
+  FASTING_V2: "ПОСТНА V.2",
+  RANCHERA: "РАНЧЕРА",
+  PIZZA_OSLO_TOMATO: "",
+  PIZZA_OSLO_CREAM: "ПИЦА ОСЛО СЪС СМЕТАНА",
+  PIZZA_GAUCHO: "ПИЦА ГАУЧО",
+};
 
-// starters
-const MOZZARELLA_BREAD = "МОЦАРЕЛЕНО ХЛЕБЧЕ";
-const MOZZARELLA_BREADSTICKS = "МОЦАРЕЛЕНИ ПРЪЧИЦИ";
+const STARTER_NAMES_CONSTANTS = {
+  MOZZARELLA_BREAD: "МОЦАРЕЛЕНО ХЛЕБЧЕ",
+  MOZZARELLA_BREADSTICKS: "МОЦАРЕЛЕНИ ПРЪЧИЦИ",
+};
 
-// chicken
-const CHICKEN_KICKERS = "ПИЛЕ КИКЕРС";
-const CHICKEN_STRIPPERS = "ПИЛЕ СТРИПЕРС";
-const BUFFALO_WINGS = "КРИЛЦА BUFFALO";
-const HOT_BUFFALO_WINGS = "КРИЛЦА BUFFALO ЛЮТИ";
+const CHICKEN_NAMES_CONSTANTS = {
+  CHICKEN_KICKERS: "ПИЛЕ КИКЕРС",
+  CHICKEN_STRIPPERS: "ПИЛЕ СТРИПЕРС",
+  BUFFALO_WINGS: "КРИЛЦА BUFFALO",
+  HOT_BUFFALO_WINGS: "КРИЛЦА BUFFALO ЛЮТИ",
+};
 
-// pasta
-const CARBONARA_PASTA = "ПАСТА КАРБОНАРА + ПРИБОРИ";
-const NAPOLITEN = "НАПОЛИТАНА + ПРИБОРИ";
-const PASTA_PEPPERONI = "ПАСТА ПЕПЕРОНИ + ПРИБОРИ";
-const MAC_AND_CHEESE = "МАК ЕНД ЧИЙЗ + ПРИБОРИ";
+const PASTA_NAMES_CONSTANTS = {
+  CARBONARA_PASTA: "ПАСТА КАРБОНАРА + ПРИБОРИ",
+  NAPOLITEN: "НАПОЛИТАНА + ПРИБОРИ",
+  PASTA_PEPPERONI: "ПАСТА ПЕПЕРОНИ + ПРИБОРИ",
+  MAC_AND_CHEESE: "МАК ЕНД ЧИЙЗ + ПРИБОРИ",
+};
 
-// salads
-const ROCKET_SALAD = "САЛАТА РОКА + СОС + ПРИБОРИ";
-const CAESARS_BACON = "САЛАТА ЦЕЗАР БЕКОН + СОС + ПРИБОРИ";
-const CAESARS_CHICKEN = "САЛАТА ЦЕЗАР С ПИЛЕ + СОС + ПРИБОРИ";
-const TUNA_SALAD = "САЛАТА РИБА ТОН + ПРИБОРИ";
+const SALAD_NAMES_CONSTANTS = {
+  ROCKET_SALAD: "САЛАТА РОКА + СОС + ПРИБОРИ",
+  CAESARS_BACON: "САЛАТА ЦЕЗАР БЕКОН + СОС + ПРИБОРИ",
+  CAESARS_CHICKEN: "САЛАТА ЦЕЗАР С ПИЛЕ + СОС + ПРИБОРИ",
+  TUNA_SALAD: "САЛАТА РИБА ТОН + ПРИБОРИ",
+};
 
-// sandwiches
-const PEPPERONI_SANDWICH = "САНДВИЧ ПЕПЕРОНИ";
-const BBQ_CHICKEN_SANDWICH = "САНДВИЧ БАРБЕКЮ ПИЛЕ";
-const MEDITERRANEO_SANDWICH = "САНДВИЧ МЕДИТЕРАНЕО";
+const SANDWICH_NAMES_CONSTANTS = {
+  PEPPERONI_SANDWICH: "САНДВИЧ ПЕПЕРОНИ",
+  BBQ_CHICKEN_SANDWICH: "САНДВИЧ БАРБЕКЮ ПИЛЕ",
+  MEDITERRANEO_SANDWICH: "САНДВИЧ МЕДИТЕРАНЕО",
+};
 
-// dips
-const FRANKS_HOT_SAUCE = "FRANK'S ЛЮТ СОС";
-const HONEY_MUSTARD_DIP  = "МЕДЕНА ГОРЧИЦА";
-const GARLIC_HERBS_DIP = "ЧЕСНОВ COC";
-const BARBECUE_DIP = "БАРБЕКЮ СОС";
-const SWEET_CHILLI_DIP = "ЧИЛИ СОС";
-const TOMATO_DIP = "ДОМАТЕН СОС";
-const MILK_BASED_SAUCE = "МЛЕЧЕН СОС";
-const RANCH_DIP = "РАНЧ СОС";
-const CAESAR_DIP = "ЦЕЗАР СОС";
-const ITALIAN_DIP = "ИТАЛИАНСКИ СОС";
+const DIP_NAMES_CONSTANTS = {
+  FRANKS_HOT_SAUCE: "FRANK'S ЛЮТ СОС",
+  HONEY_MUSTARD_DIP: "МЕДЕНА ГОРЧИЦА",
+  GARLIC_HERBS_DIP: "ЧЕСНОВ COC",
+  BARBECUE_DIP: "БАРБЕКЮ СОС",
+  SWEET_CHILLI_DIP: "ЧИЛИ СОС",
+  TOMATO_DIP: "ДОМАТЕН СОС",
+  MILK_BASED_SAUCE: "МЛЕЧЕН СОС",
+  RANCH_DIP: "РАНЧ СОС",
+  CAESAR_DIP: "ЦЕЗАР СОС",
+  ITALIAN_DIP: "ИТАЛИАНСКИ СОС",
+};
 
-// ingredients
-const SAUCES = "Сосове";
-const TOMATO_SAUCE = "Доматен сос";
-const CREAM = "Сметана";
-const BBQ = "Барбекю сос";
+const DESSERT_NAMES_CONSTANTS = {
+  CHOCO_PIE: "ЧОКО ПАЙ",
+  CHOCOLATE_SOUFFLE: "ШОКОЛАДОВО СУФЛЕ",
+  NIRVANA_PRALINES: "NIRVANA PRALINES & CREAM",
+  NIRVANA_COOKIES: "NIRVANA COOKIES & CREAM",
+  BROWNIE_BITES: "БРAУНИ ХАПКИ",
+  NIRVANA_CHOCOLATE: "NIRVANA CHOCOLATE & CHOCO CHIPS",
+  CHOCO_PIZZA: "ЧОКО ПИЦА",
+  NIRVANA_CHOCO_FUDGE: "NIRVANA CHOCO FUDGE",
+  NIRVANA_CARAMEL_FUDGE: "NIRVANA CARAMEL FUDGE",
+};
 
-const HERBS = "Подправки";
-const PARMESAN_SPRINKLES = "Пармезан снежинки";
-const BASIL = "Босилек";
-const OREGANO = "Риган";
+const DRINK_NAMES_CONSTANTS = {
+  ARYAN: "АЙРЯН 0.5L",
+  COCA_COLA_ZERO: "COCA-COLA ZERO",
+  COCA_COLA: "COCA-COLA",
+  MINERAL_WATER: "МИНЕРАЛНА ВОДА",
+  FANTA: "FANTA",
+  CAPPY_PULPY: "CAPPY PULPY",
+  FUZZTEA: "FUZZTEA",
+  CARLSBERG: "CARLSBERG 0.5L",
+  SHUMENSKO_05L: "ШУМЕНСКО 0.5L",
+  SHUMENSKO_1L: "ШУМЕНСКО 1Л",
+};
 
-const CHEESES = "Сирена";
-const EMMENTAL = "Ементал";
-const FETA = "Краве сирене";
-const MOZZARELLA = "Моцарела";
-const SMOKED_CHEESE = "Топено сирене";
-const PARMESAN = "Пармезан";
-const VEGAN_MOZZARELLA = "Веган Моцарела";
-const CHEDDAR = "Чедар сирене";
-
-const MEATS = "Меса";
-const PULLED_BEEF = "Дърпано телешко";
-const SPICY_BEEF = "Пикантно телешко";
-const SMOCKED_HAM = "Пушена шунка";
-const SMOCKED_BACON = "Пушен бекон";
-const CHORISO = "Чоризо";
-const VENTRICHINA = "Вентричина";
-const CHICKEN = "Пиле";
-const PEPPERONI = "Пеперони";
-const TUNA = "Риба тон";
-
-const VEGETABLES = "Зеленчуци";
-const ARUGULA = "Рукола";
-const PICKLES = "Кисeли краставички";
-const CORN = "Царевица";
-const ONION = "Лук";
-const BLACK_OLIVES = "Черни маслини";
-const PINAPPLE = "Ананас";
-const FRESH_TOMATOES = "Пресни домати";
-const FRESH_MUSHROOMS = "Пресни гъби";
-const FRESH_GREEN_PEPPERS = "Пресни зелени чушки";
-const JALAPENO_PEPPER = "Халапеньо-люти чушки";
-
-const MISC = "Смесен";
-const PESTO = "Песто сос";
-const CARAMELIZED_ONIONS = "Карамелизиран лук";
-
-const EXTRA = "Допълнително";
+const INGREDIENT_NAMES_CONSTANTS = {
+  SAUCES: "Сосове",
+  TOMATO_SAUCE: "Доматен сос",
+  CREAM: "Сметана",
+  BBQ: "Барбекю сос",
+  HERBS: "Подправки",
+  PARMESAN_SPRINKLES: "Пармезан снежинки",
+  BASIL: "Босилек",
+  OREGANO: "Риган",
+  CHEESES: "Сирена",
+  EMMENTAL: "Ементал",
+  FETA: "Краве сирене",
+  MOZZARELLA: "Моцарела",
+  SMOKED_CHEESE: "Топено сирене",
+  PARMESAN: "Пармезан",
+  VEGAN_MOZZARELLA: "Веган Моцарела",
+  CHEDDAR: "Чедар сирене",
+  MEATS: "Меса",
+  PULLED_BEEF: "Дърпано телешко",
+  SPICY_BEEF: "Пикантно телешко",
+  SMOCKED_HAM: "Пушена шунка",
+  SMOCKED_BACON: "Пушен бекон",
+  CHORISO: "Чоризо",
+  VENTRICHINA: "Вентричина",
+  CHICKEN: "Пиле",
+  PEPPERONI: "Пеперони",
+  TUNA: "Риба тон",
+  VEGETABLES: "Зеленчуци",
+  ARUGULA: "Рукола",
+  PICKLES: "Кисeли краставички",
+  CORN: "Царевица",
+  ONION: "Лук",
+  BLACK_OLIVES: "Черни маслини",
+  PINAPPLE: "Ананас",
+  FRESH_TOMATOES: "Пресни домати",
+  FRESH_MUSHROOMS: "Пресни гъби",
+  FRESH_GREEN_PEPPERS: "Пресни зелени чушки",
+  JALAPENO_PEPPER: "Халапеньо-люти чушки",
+  MISC: "Смесен",
+  PESTO: "Песто сос",
+  CARAMELIZED_ONIONS: "Карамелизиран лук",
+  EXTRA: "Допълнително",
+};
