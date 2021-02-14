1142151d1b2597b7d97a0e720d8a7efe2cf5e0af
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-02-14 13:06:48 +0200

    Added the general html structure of the menu and a router

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,36 @@
 let body = document.body;
 let loadingScreen = document.getElementById("loadingScreen");
+let allPages = [
+  document.getElementById("homePage"),
+  document.getElementById("allDealsPage"),
+  document.getElementById("allPizzasPage"),
+  document.getElementById("allStartersPage"),
+  document.getElementById("allChickenPage"),
+  document.getElementById("allPastaPage"),
+  document.getElementById("allSaladsPage"),
+  document.getElementById("allSandwichPage"),
+  document.getElementById("allSaucesPage"),
+  document.getElementById("allDessertsPage"),
+  document.getElementById("allDrinksPage"),
+  document.getElementById("registrationPage"),
+];
+let menuPage = document.getElementById("menuPage");
+
+function showPage() {
+  let currentPage = location.hash.slice(1);
+  if (currentPage === "") currentPage = "home";
+  let anyPageWasShown = false;
+  allPages.forEach((page) => {
+    if (page.id === currentPage + "Page") {
+      anyPageWasShown = true;
+      page.style.display = "block";
+      if (page.id.includes("all")) menuPage.style.display = "block";
+    } else {
+      page.style.display = "none";
+    }
+  });
+  //   if (!anyPageWasShown) show error page
+}
 
 function hideLoadingScreen(loadingScreen) {
   loadingScreen.style.display = "none";
@@ -14,8 +45,12 @@ function showLoadingScreen(loadingScreen) {
   setTimeout(() => hideLoadingScreen(loadingScreen), 1000);
 }
 
-window.addEventListener("DOMContentLoaded", () =>
-  showLoadingScreen(loadingScreen)
-);
+window.addEventListener("DOMContentLoaded", () => {
+  showLoadingScreen(loadingScreen);
+  showPage();
+});
 
-window.addEventListener("hashchange", () => showLoadingScreen(loadingScreen));
+window.addEventListener("hashchange", () => {
+  showLoadingScreen(loadingScreen);
+  showPage();
+});
