7be90bf1eeb2f992fbc3b6ad956f81771ef6cd6d
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-02-16 14:49:54 +0200

    modified class Product

--- assets/classes/Product.js
+++ assets/classes/Product.js
@@ -1,18 +1,8 @@
 // super class for all products(pizzas,chicken,desserts,drinks,pasta,salads,sandwiches,sauces,starters);
-<<<<<<< HEAD
 class Product{
     constructor(title,image){
         this.title=title;
         this.image=image;
     }
 }
-=======
-class Product {
-    constructor(title, image, price, weight) {
-        this.title=title;
-        this.image=image;
-        this.price=price;
-        this.weight=weight;
-    }
-}
->>>>>>> db63106cbdc84821675879bdae33e964cfc52062
+
