b98027f3c297f54515dfa57ac5c1927f1a10ebfe
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-02-23 18:44:46 +0200

    Added comments to the code

--- assets/script/productsController.js
+++ assets/script/productsController.js
@@ -8,40 +8,41 @@ function displaySimpleProduct(products, categoryTab) {
 
   categoryTab.innerHTML = html;
 
+  // selecting the cards within the current tab
   let allCards = document.querySelectorAll(
     "#" + categoryTab.id + " .simple-card"
   );
 
+  // saving the last opened section so that we can close it, when another one is opened
   let openedSection;
 
   allCards.forEach((card) => {
-    card.addEventListener(
-      "click",
-      function (ev) {
-        let index = -1;
-        ev.path.forEach((el, i) => {
-          if (el.tagName === "ARTICLE") index = i;
-        });
+    card.addEventListener("click", function (ev) {
+      // finding the actual card that was clicked from the 'event.path'
+      let index = -1;
+      ev.path.forEach((el, i) => {
+        if (el.tagName === "ARTICLE") index = i;
+      });
 
-        let slideUpSection = ev.path[index].querySelector(
-          ":scope section.slide-up"
-        );
-        if (slideUpSection.classList.contains("opened")) {
-          slideUpSection.classList.remove("opened");
-          slideUpSection.classList.add("closed");
-          openedSection = null;
-        } else {
-          slideUpSection.classList.remove("closed");
-          slideUpSection.classList.add("opened");
-          if (openedSection) {
-            openedSection.classList.remove("opened");
-            openedSection.classList.add("closed");
-          }
-          openedSection = slideUpSection;
+      // selecting the 'slide-up' section from the card
+      let slideUpSection = ev.path[index].querySelector(
+        ":scope section.slide-up"
+      );
+      if (slideUpSection.classList.contains("opened")) {
+        slideUpSection.classList.remove("opened");
+        slideUpSection.classList.add("closed");
+        openedSection = null;
+      } else {
+        slideUpSection.classList.remove("closed");
+        slideUpSection.classList.add("opened");
+        // if there is a section already opened, we close it
+        if (openedSection) {
+          openedSection.classList.remove("opened");
+          openedSection.classList.add("closed");
         }
-      },
-      true
-    );
+        openedSection = slideUpSection;
+      }
+    });
   });
 }
 
