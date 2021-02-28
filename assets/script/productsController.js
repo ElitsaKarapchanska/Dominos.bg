33d9935d10630997fab71b1fb379ca8129d5189d
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-02-28 14:33:37 +0200

    Added functionality to price

--- assets/script/productsController.js
+++ assets/script/productsController.js
@@ -13,69 +13,69 @@ sauceManager.addAllSauceProducts(allSaucesData);
 starterManager.addAllStarterProducts(allStartersData);
 
 function displaySimpleProduct(products, categoryTab) {
-  const template = Handlebars.compile(simpleProductSource);
-  const html = template(products);
+    const template = Handlebars.compile(simpleProductSource);
+    const html = template(products);
 
-  categoryTab.innerHTML = html;
+    categoryTab.innerHTML = html;
 
-  // selecting the cards within the current tab
-  let allCards = document.querySelectorAll("#" + categoryTab.id + " .simple-card");
+    // selecting the cards within the current tab
+    let allCards = document.querySelectorAll("#" + categoryTab.id + " .simple-card");
 
-  // saving the last opened section so that we can close it, when another one is opened
-  let openedSection;
+    // saving the last opened section so that we can close it, when another one is opened
+    let openedSection;
 
-  allCards.forEach((card) => {
-    card.addEventListener("click", function (ev) {
-      // finding the actual card that was clicked from the 'event.path'
-      let id = -1;
-      ev.path.forEach((el) => {
-        if (el.tagName === "ARTICLE") id = el.id;
-      });
+    allCards.forEach((card) => {
+        card.addEventListener("click", function (ev) { // finding the actual card that was clicked from the 'event.path'
+            let id = -1;
+            ev.path.forEach((el) => {
+                if (el.tagName === "ARTICLE") 
+                    id = el.id;
+                
 
-      // selecting the 'slide-up' section from the card
-      let slideUpSection = document.querySelector(
-        "#" + id + " section.slide-up"
-      );
-      if (slideUpSection.classList.contains("opened")) {
-        slideUpSection.classList.remove("opened");
-        slideUpSection.classList.add("closed");
-        openedSection = null;
-      } else {
-        slideUpSection.classList.remove("closed");
-        slideUpSection.classList.add("opened");
-        // if there is a section already opened, we close it
-        if (openedSection) {
-          openedSection.classList.remove("opened");
-          openedSection.classList.add("closed");
-        }
-        openedSection = slideUpSection;
-      }
+            });
+
+            // selecting the 'slide-up' section from the card
+            let slideUpSection = document.querySelector("#" + id + " section.slide-up");
+            if (slideUpSection.classList.contains("opened")) {
+                slideUpSection.classList.remove("opened");
+                slideUpSection.classList.add("closed");
+                openedSection = null;
+            } else {
+                slideUpSection.classList.remove("closed");
+                slideUpSection.classList.add("opened");
+                // if there is a section already opened, we close it
+                if (openedSection) {
+                    openedSection.classList.remove("opened");
+                    openedSection.classList.add("closed");
+                }
+                openedSection = slideUpSection;
+            }
+        });
     });
-  });
 }
 function showCard(hashLocation) {
-  let productPage = hashLocation.split("-");
-  let productCategory = productPage[1];
-  let productId = productPage[2];
-  switch (productCategory) {
-      case 'pizza':
-          {
-            let pizza = getProductByProductCategoryAndId(pizzaManager.allPizzas, productId);
-            displayComplexProductPage(pizza[0], allPages.complexProductPage);
-          }
-          break;
-      case 'pasta':
-          {
-            let pasta = getProductByProductCategoryAndId(pastaManager.allPasta, productId);
-            displayComplexProductPage(pasta[0], allPages.complexProductPage);
-          }
-          break;
-      case 'salad':
-          {
-            let salad = getProductByProductCategoryAndId(saladManager.allSalads, productId);
-            displayComplexProductPage(salad[0], allPages.complexProductPage);
-          }
-          break;
+    let productPage = hashLocation.split("-");
+    let productCategory = productPage[1];
+    let productId = productPage[2];
+    switch (productCategory) {
+        case 'pizza':
+            {
+                let pizza = getProductByProductCategoryAndId(pizzaManager.allPizzas, productId);
+                displayComplexProductPage(pizza[0], allPages.complexProductPage);
+            }
+            break;
+        case 'pasta':
+            {
+                let pasta = getProductByProductCategoryAndId(pastaManager.allPasta, productId);
+                displayComplexProductPage(pasta[0], allPages.complexProductPage);
+            }
+            break;
+        case 'salad':
+            {
+                let salad = getProductByProductCategoryAndId(saladManager.allSalads, productId);
+                displayComplexProductPage(salad[0], allPages.complexProductPage);
+            }
+            break;
     }
 }
 function getProductByProductCategoryAndId(data, id) {
@@ -87,14 +87,50 @@ function displayComplexProductPage(products, categoryTab) {
 
     categoryTab.innerHTML = html;
 
+    let close = document.getElementById("closeIcon");
+    close.addEventListener("click", function () {
+        window.history.back();
+    });
 
+    let quantity = document.getElementById("selectedQuantity");
+    let price = document.getElementById("currentPrice");
+    const firstPrice = document.getElementById("currentPrice");
+    let itemPrice = Number(firstPrice.innerHTML);
+
+    let minus = document.getElementById("minus");
+    minus.addEventListener("click", function () {
+
+        let count = Number(quantity.innerText);
+        let currPrice = Number(price.innerHTML);
+
+        if (count >= 2) { // check if the quantity is more than 1;
+            count--;
+
+            currPrice = currPrice - itemPrice; // to modify the price;
+            price.innerHTML = currPrice.toFixed(2);
+        }
+        quantity.innerText = count;
+
+    });
+
+    let plus = document.getElementById("plus");
+    plus.addEventListener("click", function () {
+        let count = Number(quantity.innerText);
+        let currPrice = Number(price.innerHTML);
+
+        count++;
+        currPrice = itemPrice * count; // to modify the pricel
+
+        quantity.innerText = count;
+        price.innerHTML = currPrice.toFixed(2);
+    });
 }
 
 function displayCustomizableProduct(products, categoryTab) {
-  const template = Handlebars.compile(customizableProductSource);
-  const html = template(products);
+    const template = Handlebars.compile(customizableProductSource);
+    const html = template(products);
 
-  categoryTab.innerHTML = html;
+    categoryTab.innerHTML = html;
 }
 
 displaySimpleProduct(dessertManager.allDesserts, allPages.allDessertsPage);
